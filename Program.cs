0ebb0c152d962b05d37f4cb41983891a618d82c1
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-02 08:39:25 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿void reverseString(char* s, int sSize) {
+    int len = sSize;
+    for(int x = 0; x < len/2; x++)
+    {
+        char temp = s[x];
+        s[x] = s[len - x - 1];
+        s[len - x - 1] = temp;
+//        printf("%c\n",s[x]);
+    }
+}
